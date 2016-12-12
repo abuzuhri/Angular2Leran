1620f1e49b40374f373e38abc608812c7986fbfd
Tareq Abuzuhri <tareq@dyna.mo>
2016-12-12 14:54:53 +0100

    event emiter

---
+++ angular2-learn/src/app/hearder/hearder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rb-hearder',
+  templateUrl: './hearder.component.html',
+  styles: []
+})
+export class HearderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ angular2-learn/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import  {Recipe} from '../recipe';
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styles: []
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [];
+  recipe=new Recipe('Dummy','dummy description','http://previewcf.turbosquid.com/Preview/2014/05/21__03_48_03/dummy_1.jpg73a34682-6a0e-42f8-bbeb-b21eaa495913Original.jpg');
+
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe){
+    console.log('recipe...',recipe);
+    this.recipeSelected.emit(recipe);
+  }
+}
